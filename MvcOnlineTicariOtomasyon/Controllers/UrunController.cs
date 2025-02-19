edf2415891f27e23391182531e09734f57c62935
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-02-19 14:41:59 +0300

    Bölüm 6

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var urunler = db.Uruns.Where(x=>x.Durum==true).ToList();
+            return View(urunler);
+        }
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> degerler = (from x in db.Kategoris.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.KategoriAd,
+                                                 Value = x.KategoriID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(Urun p)
+        {
+            p.Durum = true;
+            db.Uruns.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunSil(int id)
+        {
+            var urun = db.Uruns.Find(id);
+            urun.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> degerler = (from x in db.Kategoris.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.KategoriAd,
+                                                 Value = x.KategoriID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            var urunDeger = db.Uruns.Find(id);
+            return View("UrunGetir", urunDeger);
+        }
+        public ActionResult UrunGuncelle(Urun p)
+        {
+            var urn = db.Uruns.Find(p.UrunID);
+            urn.UrunAd = p.UrunAd;
+            urn.Marka = p.Marka;
+            urn.AlisFiyat = p.AlisFiyat;
+            urn.SatisFiyat = p.SatisFiyat;
+            urn.Stok = p.Stok;
+            urn.KategoriID = p.KategoriID;
+            urn.UrunGorsel = p.UrunGorsel;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
