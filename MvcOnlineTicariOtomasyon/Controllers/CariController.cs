14e0f4feb9f5da015843c46c3d9613e26b78a1ce
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-02-26 15:48:32 +0300

    Bölüm 8.1

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var degerler = db.Carilers.ToList();
+            return View(degerler);
+        }
+    }
+}
