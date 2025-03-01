04428a688450464ce88dcf09ace9d99e15100692
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-01 17:27:44 +0300

    Bölüm 9

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var degerler = db.Personels.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> degerler = (from x in db.Departmans.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.DepartmanAd,
+                                                 Value = x.DepartmanID.ToString()
+                                             }).ToList();
+            ViewBag.dgr1 = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            db.Personels.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            var personel = db.Personels.Find(id);
+            List<SelectListItem> degerler = (from x in db.Departmans.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.DepartmanAd,
+                                                 Value = x.DepartmanID.ToString()
+                                             }).ToList();
+            ViewBag.dgr1 = degerler;
+            return View("PersonelGetir", personel);
+        }
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prsnl = db.Personels.Find(p.PersonelID);
+            prsnl.PersonelAd = p.PersonelAd;
+            prsnl.PersonelSoyad = p.PersonelSoyad;
+            prsnl.PersonelGorsel = p.PersonelGorsel;
+            prsnl.DepartmanID = p.DepartmanID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
