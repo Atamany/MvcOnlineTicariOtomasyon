17a9210ad1ec11d225b79e5d6d0ccdffdb913efb
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-29 14:26:24 +0300

    Bölüm 14

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            //var degerler = db.Uruns.Where(x=>x.UrunID==1).ToList();
+            cs.Deger1=db.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Deger2 = db.Detays.Where(y => y.DetayID == 1).ToList();
+            return View(cs);
+        }
+    }
+}
