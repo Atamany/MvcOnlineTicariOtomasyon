304c189f06d283cc3c02b19ec61007626ebabce6
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-31 22:23:13 +0300

    Bölüm 19

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,29 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        Context db = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var degerler = db.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = db.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.CariID).FirstOrDefault();
+            var degerler = db.SatisHarekets.Where(x => x.CariID == id).ToList();
+            return View(degerler);
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -26,6 +26,10 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [StringLength(50, ErrorMessage = "En fazla 50 karakter yazabilirsiniz.")]
         [Required(ErrorMessage = "Bu alanı boş geçemezsiniz.")]
         public string CariMail { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(20, ErrorMessage = "En fazla 20 karakter yazabilirsiniz.")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz.")]
+        public string CariSifre { get; set; }
         public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
